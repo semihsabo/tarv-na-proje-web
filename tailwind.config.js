8b0eeb146fe6b3aa3df80495ecb869784f7d3f2f
Semih Sabo <seninmailin@ornek.com>
2025-07-08 18:50:02 +0300

    Yeni başlangıç

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+};
